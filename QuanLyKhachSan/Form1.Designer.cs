2df77c7c277395cbc44c6fc03c01d605060ea187
quang <doquang1996@gmail.com>
2017-05-12 18:02:56 +0700

    còn thiếu và sai chỗ nào, ae sửa giùm nha!

---
+++ QuanLyKhachSan/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.groupBox1.Location = new System.Drawing.Point(23, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 175);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin đăng nhập";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(157, 41);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(206, 26);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(157, 79);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(206, 26);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(53, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tên đăng nhập";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(53, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật khẩu";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(180, 226);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(311, 226);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(84, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 296);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormLogin";
+            this.Text = "Đăng nhập hệ thống";
+            this.TopMost = true;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ QuanLyKhachSan/TraPhong.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QuanLyKhachSan.Model;
+
+namespace QuanLyKhachSan
+{
+    public partial class TraPhong : Form
+    {
+        QuanLyKhachSanEntities3 db = new QuanLyKhachSanEntities3();
+        IEnumerable<MaPhieuDK> tk;
+        int songay = 0;
+        int tienphong = 0;
+        int tiendichvu = 0;
+        public TraPhong()
+        {
+            InitializeComponent();
+        }
+
+        private void TraPhong_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string tkSDT = txtTiemKiemSDT.Text;
+            int tkMaPDK;
+            tk = from b in db.MaPhieuDKs select b;
+
+            string tkname = txtTiemKiemName.Text;
+            string cmnd = txtTimKiemCMND.Text;
+            DateTime ngayden = dateTimePicker1.Value;
+
+            if (txtTimKiemMaPDK.Text != "")
+            {
+                if (!int.TryParse(txtTimKiemMaPDK.Text, out int so))
+                {
+                    MessageBox.Show("Nhap dung ma phieu dang ki !");
+                    return;
+                }
+                else
+                {
+                    tkMaPDK = so;
+                    tk = from p in tk where p.MaPDK == tkMaPDK select p;
+                }
+            }
+            if (tkname != "")
+            {
+                tk = from p in tk where p.KhachHang.TenKH == tkname select p;
+            }
+            if (tkSDT != "")
+            {
+                tk = from p in tk where p.KhachHang.SDT == tkSDT select p;
+            }
+            if (dateTimePicker1.Checked)
+            {
+                tk = from p in tk where p.NgayDen.Value.Date == ngayden.Date select p;
+            }
+            dataGridView1.DataSource = tk.ToList();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            button1.Enabled = true;
+            button3.Enabled = true;
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            tienphong = 0;
+            tiendichvu = 0;
+            songay = 0;
+
+            int madk = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString().TrimEnd());
+            var phieu = db.MaPhieuDKs.SingleOrDefault(p => p.MaPDK == madk);
+            txtDatTruoc.Text = phieu.TraTruoc.ToString();
+           var  ngay = DateTime.Now.Date - phieu.NgayDen.Value.Date ;
+            songay = ngay.Days;
+            textBox4.Text = songay.ToString();
+            var dichvu = phieu.DichVus;
+            foreach (var y in dichvu)
+            {
+                tiendichvu = tiendichvu + y.GiaDV.Value * songay;
+            }
+            var phong = phieu.Phongs;
+
+            foreach (var t in phong)
+            {
+                tienphong = +t.GiaPhong.Gia.Value * songay;
+            }
+            txtTienDV.Text = tiendichvu.ToString();
+            txtTienPhong.Text = tienphong.ToString();
+            txtSum.Text = (tienphong + tiendichvu).ToString();
+            textBox1.Text = (tiendichvu + tienphong - phieu.TraTruoc).ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                textBox3.Text = (-int.Parse(textBox1.Text)).ToString();
+            }
+            else if (int.TryParse(textBox2.Text, out int a))
+            {
+                textBox3.Text = (a - int.Parse(textBox1.Text)).ToString();
+            }
+            else
+            {
+                MessageBox.Show("Xin nhap so vao o tien nhan!");
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int madk = int.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString().TrimEnd());
+            var phieu = db.MaPhieuDKs.SingleOrDefault(p => p.MaPDK == madk);
+            phieu.NgayDi = DateTime.Now;
+            var phieutt = new QuanLyKhachSan.Model.PhieuTT();
+            phieutt.MaPDK = phieu.MaPDK;
+            phieutt.NgayTT = DateTime.Now;
+            phieutt.SoNgay = DateTime.Now.Day - phieu.NgayDen.Value.Day;
+            phieutt.TongTien = int.Parse(textBox1.Text);
+            db.PhieuTTs.Add(phieutt);
+            db.SaveChanges();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            tienphong = 0;
+            tiendichvu = 0;
+            songay = 0;
+            tk = null;
+            TraPhong_Load(sender, e);
+            txtDatTruoc.Clear();
+            txtSum.Clear();
+            txtTiemKiemName.Clear();
+            txtTiemKiemSDT.Clear();
+            txtTienDV.Clear();
+            txtTienPhong.Clear();
+            txtTimKiemCMND.Clear();
+            txtTimKiemMaPDK.Clear();
+            textBox1.Clear();
+        }
+    }
+}
